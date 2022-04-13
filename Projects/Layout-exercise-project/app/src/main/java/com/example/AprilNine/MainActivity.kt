dd5c6d779535fb574c2e082aef11aae5cf52ef5c
Dilay Erem <karabagdilayerem@gmail.com>
2022-04-14 01:41:17 +0300

    Bu proje eklendi

---
+++ Projects/Layout-exercise-project/app/src/main/java/com/example/AprilNine/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.AprilNine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
